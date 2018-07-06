badf0ec61406eff3d316f9b16d916b81c87fa8e6
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-07-06 15:18:37 +0800

    fungsi login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
@@ -9,7 +8,6 @@
   <title>Angle - Bootstrap Admin Template</title>
   <link rel="stylesheet" href="{{asset('css/app.css')}}" id="maincss">
 </head>
-
 <body>
   <div class="wrapper">
     <div class="block-center mt-xl wd-xl">
@@ -21,30 +19,30 @@
         </div>
         <div class="panel-body">
           <p class="text-center pv">Login Untuk Melanjutkan</p>
-          <form class="mb-lg" role="form" data-parsley-validate="" novalidate="">
+          <form class="mb-lg" role="form" data-parsley-validate="" novalidate="" method="POST" action="{{ route('Login') }}">
+            @csrf
             <div class="form-group has-feedback">
-              <input class="form-control" id="exampleInputEmail1" type="text" placeholder="Enter email" autocomplete="off" required>
-              <span class="fa fa-envelope form-control-feedback text-muted"></span>
+              <input class="form-control" id="exampleInputEmail1" type="text" placeholder="Username" autocomplete="off" name="username" required>
+              <span class="fa fa-user form-control-feedback text-muted"></span>
             </div>
             <div class="form-group has-feedback">
-              <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password" required>
+              <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password" name="password" required>
               <span class="fa fa-lock form-control-feedback text-muted"></span>
             </div>
-              <button class="btn btn-block btn-primary mt-lg" type="submit">Login</button>
-            </form>
-          </div>
-        </div>
-        <div class="p-lg text-center">
-          <span>&copy;</span>
-          <span>2017</span>
-          <span>-</span>
-          <span>Angle</span>
-          <br>
-          <span>Bootstrap Admin Template</span>
+            <button class="btn btn-block btn-primary mt-lg" type="submit">Login</button>
+          </form>
         </div>
       </div>
+      <div class="p-lg text-center">
+        <span>&copy;</span>
+        <span>2017</span>
+        <span>-</span>
+        <span>Angle</span>
+        <br>
+        <span>Bootstrap Admin Template</span>
+      </div>
     </div>
-    <script src="{{asset('js/app.js')}}"></script>
-  </body>
-
-  </html>
+  </div>
+  <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::GET('login', 'Auth\LoginController@showLoginForm')->name('LoginForm');
+Route::POST('login', 'Auth\LoginController@login')->name('Login');
+Route::GET('logout', 'Auth\LoginController@logout')->name('Logout');
 
-Route::get('/', function () {
-    return view('User.Dashboard');
+Route::group(['middleware' => ['AuthMiddleware']], function () {
+  Route::GET('', 'UserController@Dashboard');
 });
-
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
