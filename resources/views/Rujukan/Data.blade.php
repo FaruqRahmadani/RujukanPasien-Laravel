d2ecf04d60e9492ae77ed975a38ff141d5991233
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-07-17 00:35:45 +0800

    data rujukan

--- resources/views/Rujukan/Data.blade.php
+++ resources/views/Rujukan/Data.blade.php
@@ -12,24 +12,28 @@
                   <th>Nomor RM</th>
                   <th>Nama</th>
                   <th>Umur</th>
-                  <th>Poli</th>
+                  <th>Poli Dari</th>
+                  <th>Poli Tujuan</th>
                   <th>Dokter</th>
                   <th>Keluhan</th>
                   <th>Diagnosa</th>
+                  <th>Tanggal</th>
                   <th>Action</th>
                 </tr>
               </thead>
               <tbody>
                 @foreach ($Pasien as $Index=>$DataPasien)
                   <tr>
-                    <td>{{$Index=+1}}</td>
+                    <td>{{$Index+1}}</td>
                     <td>{{$DataPasien->nomor_rm}}</td>
                     <td>{{$DataPasien->nama}}</td>
-                    <td>{{$DataPasien->umur}}</td>
-                    <td>{{$DataPasien->Dokter->Spesialis->nama}}</td>
+                    <td>{{$DataPasien->umur}} Tahun</td>
+                    <td>{{$DataPasien->PoliDari->nama}}</td>
+                    <td>{{$DataPasien->PoliTujuan->nama}}</td>
                     <td>{{$DataPasien->Dokter->nama}}</td>
                     <td>{{$DataPasien->keluhan}}</td>
                     <td>{{$DataPasien->diagnosa}}</td>
+                    <td>{{HTanggal::Format($DataPasien->created_at)}}</td>
                     <td>
                       <a href="{{Route('Respon-Rujukan', ['Id' => HCrypt::Encrypt($DataPasien->id)])}}" class="btn btn-labeled btn-info btn-xs">
                         <span class="btn-label"><i class="fa fa-exclamation-circle"></i>
